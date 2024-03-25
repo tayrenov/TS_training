648129a7841f6a501a6d2d1af7759983e54cded5
Aleksey Korovin <tayrenov@gmail.com>
2024-03-25 19:53:53 +0300

    advenced types upg + practica

--- advanced-types_info/app.js
+++ advanced-types_info/app.js
@@ -1,5 +1,14 @@
 "use strict";
 /********** UNION **********/
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 function logId(id) {
     if (typeof id === 'string') {
         console.log('String_' + id);
@@ -51,4 +60,105 @@ logMultipleIds(1, '1');
 logMultipleIds('1', '1');
 logMultipleIds('2', '1');
 console.log('____________');
+/***************************/
+/********** LITERAL **********/
+function fetchWithAuth(url, method) {
+    console.log(url);
+    console.log(method);
+    return 1;
+}
+fetchWithAuth('str', 'post');
+fetchWithAuth('str', 'get');
+console.log('____________');
+let user = {
+    name: 'name',
+    age: 12,
+    skills: ['s1', 's2'],
+    id: 1
+};
+console.log(user);
+console.log('____________');
+let user1 = {
+    name: 'str',
+    age: 1,
+    skills: ['1', '2'],
+    id: 1,
+    createdAt: new Date()
+};
+console.log(user1);
+console.log('____________');
+const user2 = {
+    login: 'mail@com',
+    password: '1'
+};
+function multiply(first, second) {
+    if (!second) {
+        return first * first;
+    }
+    return first * second;
+}
+console.log(multiply(5));
+console.log(multiply(5, 3));
+console.log('____________');
+function testPass(user) {
+    var _a;
+    const t = (_a = user.password) === null || _a === void 0 ? void 0 : _a.type;
+    return t;
+}
+function test(param) {
+    const t = param !== null && param !== void 0 ? param : multiply(5);
+    return t;
+}
+console.log(testPass({ login: 'login',
+    password: {
+        type: 'secondary'
+    }
+}));
+console.log(testPass({ login: 'login' }));
+console.log(test('str'));
+console.log(test());
+console.log('____________');
+/***************************/
+/********** UNKNOWN **********/
+let input;
+input = 3;
+function run(i) {
+    if (typeof i == 'number') {
+        i++;
+    }
+    return i;
+}
+console.log(run(3));
+console.log(run('str'));
+function getData() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield fetch('');
+        }
+        catch (error) {
+            if (error instanceof Error) {
+                console.log(error.message);
+            }
+        }
+    });
+}
+function getDataForce() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield fetch('');
+        }
+        catch (error) {
+            const e = error;
+            console.log(e.message);
+        }
+    });
+}
+getData();
+getDataForce();
+/***************************/
+/********** NEVER **********/
+function generateError(message) {
+    console.log(message);
+}
+generateError('message');
 /***************************/ 
--- advanced-types_info/app.ts
+++ advanced-types_info/app.ts
@@ -60,3 +60,170 @@ logMultipleIds('2', '1')
 console.log('____________')
 
 /***************************/
+
+/********** LITERAL **********/
+
+function fetchWithAuth(url: string, method: 'post' | 'get'): 1 | -1 {
+    console.log(url);
+    console.log(method);
+    return 1;
+}
+
+fetchWithAuth('str', 'post')
+fetchWithAuth('str', 'get')
+
+console.log('____________')
+
+/***************************/
+
+/********** TYPE **********/
+
+type User = {
+    name: string,
+    age: number,
+    skills: string[],
+    id: number
+}
+
+let user: User = {
+    name: 'name',
+    age: 12,
+    skills: ['s1', 's2'],
+    id:1
+}
+
+console.log(user)
+
+console.log('____________')
+
+/***************************/
+
+/********** INTERFACE **********/
+
+interface User1 {
+    name: string,
+    age: number,
+    skills: string[],
+
+}
+
+interface UserID {
+    id: number
+}
+
+interface CreateUser extends User1, UserID {
+    createdAt: Date;
+}
+
+let user1: CreateUser = {
+    name: 'str',
+    age: 1,
+    skills: ['1','2'],
+    id: 1,
+    createdAt: new Date()
+}
+
+console.log(user1)
+
+console.log('____________')
+
+/***************************/
+
+/********** OPTIONAL **********/
+
+interface User2 {
+    login: string;
+    password?: string;
+}
+
+const user2: User2 = {
+    login:'mail@com',
+    password: '1'
+}
+
+function multiply(first: number, second?: number): number {
+    if (!second) {
+        return first * first
+    }
+
+    return first * second
+}
+
+console.log(multiply(5))
+console.log(multiply(5,3))
+
+console.log('____________')
+
+interface UserPro {
+    login: string;
+    password?: {
+        type: 'primary' | 'secondary'
+    }
+}
+
+function testPass(user: UserPro) {
+    const t = user.password?.type;
+    return t;
+}
+
+function test(param?: string) {
+    const t = param ?? multiply(5);
+    return t;
+}
+
+console.log(testPass(
+    {login:'login', 
+        password:{
+        type:'secondary'
+        }
+    }))
+
+console.log(testPass({login:'login'}))
+
+console.log(test('str'))
+console.log(test())
+
+console.log('____________')
+/***************************/
+
+/********** UNKNOWN **********/
+
+let input: unknown;
+
+input = 3;
+
+function run(i: unknown) {
+    if (typeof i == 'number') {
+        i++
+    } 
+
+    return i
+}
+
+console.log(run(3))
+console.log(run('str'))
+
+async function getData() {
+    try {
+        await fetch('');
+    } catch (error) {
+        if (error instanceof Error) {
+            console.log(error.message)
+        }
+    }
+}
+
+async function getDataForce() {
+    try {
+        await fetch('');
+    } catch (error) {
+        const e = error as Error;
+        console.log(e.message)
+    }
+}
+
+getData()
+
+getDataForce()
+
+/***************************/
